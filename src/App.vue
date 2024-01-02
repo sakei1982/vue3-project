7d75b8fc37f65a3d148871e1209207446312df9c
sakei <29232771@qq.com>
2024-01-02 19:57:06 +0900

    ref使用

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
-<!-- <script>
-  export default{
-    setup(){
-      console.log('setup',this)
-      const message = 'thishello'
-      const logMessage = () => {
-        console.log(message)
-      }
-      return{
-        message,
-        logMessage
-      }
-    },
-  beforeCreate(){
-    console.log('beforeCreate')
-  }
-  }
-</script> -->
 <script setup>
-const message = 'this'
-const logMessage = () => {
-  console.log(message)
+// //导入函数
+// import { reactive } from 'vue';
+
+// //执行函数 传入对象类型参数 变量接受
+// const state = reactive({
+//   count:0
+// })
+// const setCount = ()=>{
+//   state.count++
+// }
+//导入函数
+import { ref } from 'vue'
+//执行函数 传入参数【简单类型 对象类型】 变量接受
+const count = ref({
+  count:0
+})
+const setCount = ()=>{
+  //修改数据需要加.value
+  count.value++
 }
 </script>
+
 <template>
   <div>
-    {{ message }}
-    <button @click="logMessage">log</button>
+    <!-- <button @click="setCount">{{ state.count }}</button> -->
+    <button @click="setCount">{{ count.value }}</button>
   </div>
 </template>
