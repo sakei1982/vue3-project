79ebf26b3458f52c4435403534758aae12b5f8f1
sakei <29232771@qq.com>
2024-01-02 17:34:16 +0900

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
+<!-- <script>
+  export default{
+    setup(){
+      console.log('setup',this)
+      const message = 'thishello'
+      const logMessage = () => {
+        console.log(message)
+      }
+      return{
+        message,
+        logMessage
+      }
+    },
+  beforeCreate(){
+    console.log('beforeCreate')
+  }
+  }
+</script> -->
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const message = 'this'
+const logMessage = () => {
+  console.log(message)
+}
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    {{ message }}
+    <button @click="logMessage">log</button>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
